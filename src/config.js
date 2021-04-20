b6b9eeb62b7cff1573072d276d1ed518091c14ed
crowfather <lukau2357@gmail.com>
2021-04-20 17:27:24 +0200

    Added openup-backend Cognito User Pool integration

--- src/config.js
+++ src/config.js
@@ -3,9 +3,8 @@
 const config = {
     cognito: {
       REGION: "eu-central-1",
-      USER_POOL_ID: "eu-central-1_c23miOAYW",
-      APP_CLIENT_ID: "31o2p3ofa1u61edh3ogagmffvf",
-      // IDENTITY_POOL_ID: "YOUR_IDENTITY_POOL_ID"
+      USER_POOL_ID: "eu-central-1_TlF0a7fko",
+      APP_CLIENT_ID: "7d3ok998bhcuc9g9kdr9l9mp13",
      }
   };
   
