2fdcef85edf2bd362c271e9e368052cd0e49a7c8
Ognjen Vukotic <ognjendev.3@gmail.com>
2021-05-30 19:02:38 +0200

    make item header change

--- src/app/components/Proizvod.jsx
+++ src/app/components/Proizvod.jsx
@@ -237,19 +237,9 @@ const Proizvod = () => {
         {/* slika */}
 
         {/* naslov */}
-        <div className="font-regular text-2xl flex items-baseline justify-between">
+        <div className="font-regular text-2xl flex items-baseline justify-start">
           <p>{name}</p>
-          <div className="text-gray-500">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
-              <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" />
-            </svg>
-          </div>
+
           {/* naslov */}
         </div>
         {/* slika + naslov */}
